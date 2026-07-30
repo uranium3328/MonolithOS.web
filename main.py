c7c38a47f144e91d25a2d80492bda4248fd26589
uranium3328 <231964438+uranium3328@users.noreply.github.com>
2026-07-30 12:31:50 +0300

    Перемеинован сайт, добавлены маленькие изменения

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def general():
-    return render_template("index.html")
+@app.route('/')
+def main_page():
+    return render_template('index.html')
 
-@app.route("/about")
+@app.route('/post')
+def post_page():
+    return render_template('post.html')
+
+@app.route('/post2')
+def post2_page():
+    return render_template('post2.html')
+
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
